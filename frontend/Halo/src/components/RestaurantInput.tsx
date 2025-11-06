64046985b93f79022e67d5245857610e5a13d359
dolphinalt <ethan.zhao.us@gmail.com>
2025-11-05 23:24:50 -0800

    basic layout of resturant input

--- frontend/Halo/src/components/RestaurantInput.tsx
+++ frontend/Halo/src/components/RestaurantInput.tsx
@@ -2,45 +2,51 @@ import React from "react";
 
 const RestaurantInput = () => {
   return (
-    <div className="h-full w-full bg-white/50 rounded-3xl shadow-xl backdrop-blur-sm border border-white/50 flex flex-col items-center justify-center gap-4 p-8">
-      <button className="w-64 h-12 bg-[#b5d3dc] border border-white/50 rounded-2xl flex items-center justify-center gap-3 hover:bg-[#a5c3cc] transition-colors">
-        <span className="text-black text-2xl font-sf-pro">Upload Menu</span>
-        <svg
-          width="32"
-          height="32"
-          viewBox="0 0 32 32"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle cx="16" cy="16" r="16" fill="#56BECC80" fillOpacity="1" />
-          <path
-            //M16 10V22M10 16H22 is the plus icon just fyi
-            d="M16 10V22M10 16H22"
-            stroke="white"
-            strokeWidth="4"
-            strokeLinecap="round"
-          />
-        </svg>
-      </button>
-
-      <button className="w-64 h-12 bg-white/50 border border-white/50 rounded-2xl flex items-center justify-center gap-3 hover:bg-[#D3D3D3] transition-colors">
-        <span className="text-black text-2xl font-sf-pro">Manual Input</span>
-        <svg
-          width="32"
-          height="32"
-          viewBox="0 0 32 32"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle cx="16" cy="16" r="16" fill="#56BECC80" fillOpacity="1" />
-          <path
-            d="M16 10V22M10 16H22"
-            stroke="white"
-            strokeWidth="4"
-            strokeLinecap="round"
-          />
-        </svg>
-      </button>
+    <div className="h-full w-full bg-white/50 rounded-3xl shadow-xl backdrop-blur-sm border border-white/50 flex flex-col items-center justify-center gap-4 p-4">
+      <div className="grid grid-cols-[2fr_1fr] gap-[1rem] items-start w-full h-full">
+        <div className="h-full w-full rounded-xl shadow-xl backdrop-blur-sm outline outline-1 outline-offset-[-0.0625rem] outline-white/50 p-6"></div>
+        <div className="grid grid-rows-2 gap-[1rem] items-start w-full h-full">
+          <button className="w-full h-full bg-[#b5d3dc] border border-white/50 rounded-2xl flex items-center justify-center gap-3 hover:bg-[#a5c3cc] transition-colors">
+            <span className="text-black text-2xl font-sf-pro">Upload Menu</span>
+            <svg
+              width="32"
+              height="32"
+              viewBox="0 0 32 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle cx="16" cy="16" r="16" fill="#56BECC80" fillOpacity="1" />
+              <path
+                //M16 10V22M10 16H22 is the plus icon just fyi
+                d="M16 10V22M10 16H22"
+                stroke="white"
+                strokeWidth="4"
+                strokeLinecap="round"
+              />
+            </svg>
+          </button>
+          <button className="w-full h-full bg-white border border-white/50 rounded-2xl flex items-center justify-center gap-3 hover:bg-[#a5c3cc] transition-colors">
+            <span className="text-black text-2xl font-sf-pro">
+              Manual Input
+            </span>
+            <svg
+              width="32"
+              height="32"
+              viewBox="0 0 32 32"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle cx="16" cy="16" r="16" fill="#56BECC80" fillOpacity="1" />
+              <path
+                d="M16 10V22M10 16H22"
+                stroke="white"
+                strokeWidth="4"
+                strokeLinecap="round"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
